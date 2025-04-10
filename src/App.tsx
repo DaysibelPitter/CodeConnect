620ede8892df86bd3e28e5500bce755b018002c2
daysibel1175 <estergutierres1175@gmail.com>
2025-04-10 10:05:09 -0300

    mejorando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login'
+import Cadastro from './components/Cadastro/Cadastro'
+// import Feed from './components/Feed/Feed'
+
 function App() {
  
 
   return (
     <>
-      <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro/>} />
+        {/* <Route path="/feed" element={<Feed />} /> */}
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+
