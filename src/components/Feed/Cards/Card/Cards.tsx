ee35d6094ee645104aea31f67a9ee36bd0d4c682
daysibel1175 <estergutierres1175@gmail.com>
2025-05-20 09:25:49 -0300

    cards conectada a redux

--- src/components/Feed/Cards/Card/Cards.tsx
+++ src/components/Feed/Cards/Card/Cards.tsx
@@ -1,39 +1,44 @@
+import { Link } from "react-router-dom";
 import "./Cards.css";
+import img from "../../../../assets/imagenCard.png";
 import SocialEngagement from "../SocialEngagement/SocialEngagement";
-import img from "../../../../assets/imagenCard.png"; 
-function Cards() {
-    const cardsData = [
-        { usuario: "Carlos", title: "Card 1", description: "Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "4", totalCompartidos: "26", totalSalvos: "3" },
-        { usuario: "Daniel", title: "Card 2", description: "Otra descripción interesante.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "40", totalCompartidos: "12", totalSalvos: "43" },
-        { usuario: "Carlos", title: "Card 1", description: "Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "4", totalCompartidos: "26", totalSalvos: "3" },
-        { usuario: "Daniel", title: "Card 2", description: "Otra descripción interesante.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "40", totalCompartidos: "12", totalSalvos: "43" },
-        { usuario: "Carlos", title: "Card 1", description: "Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "4", totalCompartidos: "26", totalSalvos: "3" },
-        { usuario: "Daniel", title: "Card 2", description: "Otra descripción interesante.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.Descripción de la primera card.", totalComentarios: "40", totalCompartidos: "12", totalSalvos: "43" },
-    ];
+import { Proyecto } from "../../../../Redux/sliceProjects";
 
-    return (
-        <div className="card-container">
-            {cardsData.map((card, index) => (
-                <div key={index} className="card">
-                    <div className="card-img">
-                        <img src={img} alt={card.usuario} /> 
-                    </div>
-                    <div className="card-content">
-                        <div className="card-text">
-                            <h1>{card.title}</h1>
-                            <p className="card-description">{card.description}</p>   
-                        </div>
-                        <SocialEngagement 
-                            usuario={card.usuario}
-                            totalComentarios={card.totalComentarios}
-                            totalCompartidos={card.totalCompartidos}
-                            totalSalvos={card.totalSalvos}
-                        />
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+interface CardsProps {
+  proyectos: Proyecto[];
+}
+
+function Cards({ proyectos }: CardsProps) {
+  return (
+    <div className="card-container">
+      {proyectos.map((card, index) => (
+        <Link 
+          key={index} 
+          to={`/detailsprojects/${card.id}`} 
+          state={{ proyecto: card }}
+          className="card-link"
+        >
+          <div className="card">
+            <div className="card-img">
+              <img src={img} alt={card.usuario} />
+            </div>
+            <div className="card-content">
+              <div className="card-text">
+                <h1>{card.nombre}</h1>
+                <p className="card-description">{card.descripcion}</p>
+              </div>
+              <SocialEngagement
+                usuario={card.usuario}
+                totalComentarios={card.totalComentarios}
+                totalCompartidos={card.totalCompartidos}
+                totalSalvos={card.totalSalvos}
+              />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Cards;
