24914adec9f9978775c421b51d5fa90eebf3b44d
daysibel1175 <estergutierres1175@gmail.com>
2025-06-02 09:52:39 -0300

    aplicando componente boton

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -5,6 +5,7 @@ import { FaInfoCircle } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaArrowRightFromBracket } from "react-icons/fa6";
+import Boton from "../../components/Boton/Boton";
 // icone de login = import { FaArrowRightToBracket } from "react-icons/fa6";
 // <FaArrowRightToBracket />
 function NavFeed() {
@@ -13,7 +14,7 @@ function NavFeed() {
     <>
     <nav className='sidebar'>
     <img src={logo} alt="logo" />
-    <Link to="/publicar" className='botaoPublicar'>Publicar</Link>
+    <Link to="/publicar"><Boton texto='Publicar'/></Link>
     <Link to="/feed" className='navBotao'><FaRegFileAlt />Feed</Link >
     <Link to="/perfil"  className='navBotao'><FaUserCircle />Perfil</Link >
     <Link to="/acercaDe"  className='navBotao'><FaInfoCircle />Sobre nós</Link >
