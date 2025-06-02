2243343a6c60e76c35e4d0d4eaf9d7ffb82a6f9d
daysibel1175 <estergutierres1175@gmail.com>
2025-06-02 09:52:12 -0300

    creando componente de boton

---
+++ src/components/Boton/Boton.tsx
@@ -0,0 +1,21 @@
+import "./boton.css";
+
+interface BotonProps {
+  texto: string;
+  colorFondo?: string; 
+  colorTexto?: string;
+  tipo?: "submit" | "button" | "reset";
+  className?: string;
+  icone?: React.ReactNode;
+}
+
+const Boton = ({ texto, colorFondo = "", colorTexto="#81FE88" , icone}: BotonProps) => {
+  return (
+    <button className="boton" style={{ backgroundColor: colorFondo, color: colorTexto }}>
+      {texto}
+    {icone && <span className="icone">{icone}</span>} 
+    </button>
+  );
+};
+
+export default Boton;
